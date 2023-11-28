7d77fd4885225c0ca8841aacc3407176e736f64c
Nathan Le Roux <nleroux930@gmail.com>
2023-11-28 11:11:09 +0800

    pre pyright tweaking

---
+++ .config/nvim/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int some_var = 0;
+    some_var += 2;
+
+    return EXIT_SUCCESS;
+}
---
+++ .config/nvim/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def print_line(num):
+    for x in range:
+        print(x)
+
+def main(num):
+    for x in num:
+        print_line(x)
+
+if __name__ == "__main__":
+    main(10)
+    
